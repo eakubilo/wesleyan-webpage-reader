444499021b9fe1bccaaf07aef2d24020390bdf40
Chukwuebuka Akubilo <chukwuebukaakubilo@cssi-online.co>
2020-08-09 02:15:49 -0400

    Added regular expression to parse through html string and return course course titles.

--- public/client.js
+++ public/client.js
@@ -1,9 +1,18 @@
 let socket = io.connect('http://localhost:3000');
 
 socket.on('button', printData);
-
+let str;
+let regexp = /[A-Z]{4}[1-9]{3}[-][0][1]/gi
 function printData(data){
-    console.log(data);
+    str = data.text;
+    let match, matches =[];
+    while((match = regexp.exec(data.text)) != null){
+        matches.push({
+            index: match.index,
+            content: data.text.substring(match.index, (match.index + 7))
+        });
+    }
+    console.log(matches);
 }
 
 document.getElementById("click").onclick = clickFunction;
