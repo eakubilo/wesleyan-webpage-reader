48bb09f49e8e2f683089e1c0f0ab0b72b985c108
Chukwuebuka Akubilo <chukwuebukaakubilo@cssi-online.co>
2020-08-09 00:14:00 -0400

    Added server functionality

---
+++ index.js
@@ -0,0 +1,25 @@
+let express = require('express');
+
+let app = express();
+
+let server = app.listen(3000);
+app.use(express(static('/public')));
+
+console.log("My socket server is running");
+
+let io = socket(server);
+
+io.sockets.on('connection', newConnection);
+let req = new XMLHttpRequest();
+
+function newConnection(socket) {
+    console.log('new connection' + socket.id);
+    socket.on('button', sendRequest);
+    function sendRequest(data){
+        req.open('GET', data, false);
+        req.send(null);
+        if(req.status == 200) {
+            io.sockets.emit('button', {text: req.responseText});
+        }
+    }
+}
